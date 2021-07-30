4aaf730efe17b776d3721afbcfc9be9d035194ef
Rhythm Varshney <rhythm-design>
2021-07-30 21:05:38 +0530

    Authentication improved using passport

--- app.js
+++ app.js
@@ -3,24 +3,37 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 
-// Level-4 security npm package->
-const bcrypt=require("bcrypt");
-const saltRounds=10;
+// Advanced security using passport->
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
+require('dotenv').config();
+
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
-
+app.use(session({
+  secret:process.env.SECRET,
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/usersDB",{useNewUrlParser:true, useUnifiedTopology:true});
-
+mongoose.set("useCreateIndex",true);
 const userSchema=new mongoose.Schema({
   email:String,
   password:String
 },{versionKey: false});
 
+userSchema.plugin(passportLocalMongoose);
 
 const User=new mongoose.model("User",userSchema);
+ passport.use(User.createStrategy());
+ passport.serializeUser(User.serializeUser());
+ passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
   res.render("home");
@@ -40,41 +53,29 @@ app.get("/secrets",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  // Level-4 Security case using bcrypt->
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser=new User({
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }else{
-        res.render("secrets");
-      }
-    });
+  User.register({username:req.body.username},req.body.password,function(err, foundUser){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets")
+      });
+    }
   });
-
 });
 app.post("/login",function(req,res){
-  // Level-4 Advanced Security Enhancement using bcrypt->
-  const userEmail= req.body.username;
-  const password=req.body.password;
-  User.findOne({email:userEmail},function(err,foundUser){
+  const userLogin=new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(userLogin,function(err){
     if(err){
       console.log(err);
     }else{
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-            if(result==true){
-              res.render("secrets");
-            }else{
-              res.send("<h1>Password is incorrect</h1>");
-            }
-        });
-      }else{
-        res.send("<h1>User not found, register first</h1>");
-      }
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets")
+      });
     }
   });
 });
