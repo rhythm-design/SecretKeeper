d338791805d787a392d51de1b180aaa99c14aada
Rhythm Varshney <rhythm-design>
2021-07-30 20:31:09 +0530

    Added more security using bcrypt

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 
-// Level-3 security npm package->
-const md5=require("md5");
-
+// Level-4 security npm package->
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
+
 mongoose.connect("mongodb://localhost:27017/usersDB",{useNewUrlParser:true, useUnifiedTopology:true});
 
 const userSchema=new mongoose.Schema({
   email:String,
   password:String
 },{versionKey: false});
+
+
 const User=new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
@@ -28,34 +31,47 @@ app.get("/login",function(req,res){
 app.get("/register",function(req,res){
   res.render("register");
 });
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
+});
+
 app.post("/register",function(req,res){
-  //  Level-3 Security Case
-  const newUser=new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  // Level-4 Security case using bcrypt->
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser=new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
+
 });
 app.post("/login",function(req,res){
-  // Level-3 Security using md5
+  // Level-4 Advanced Security Enhancement using bcrypt->
   const userEmail= req.body.username;
-  const password=md5(req.body.password);
+  const password=req.body.password;
   User.findOne({email:userEmail},function(err,foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
-        }else{
-          res.send("<h1>Password is incorrect</h1>");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result==true){
+              res.render("secrets");
+            }else{
+              res.send("<h1>Password is incorrect</h1>");
+            }
+        });
       }else{
         res.send("<h1>User not found, register first</h1>");
       }
